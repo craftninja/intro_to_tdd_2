366a3023057bbc21fcb1821772c6c8569ab79f18
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-11 17:46:44 -0600

    will return an array replacing mult of 3 with Fizz, 5 with Buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -14,6 +14,7 @@ class FizzBuzz
     i=0
     while i<@max_num
       fizz_buzz[i] = "Fizz" if fizz_buzz[i] % 3 == 0
+      fizz_buzz[i] = "Buzz" if fizz_buzz[i] % 5 == 0
       i+=1
     end
     fizz_buzz
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,8 +2,8 @@ require "fizz_buzz"
 require "rspec/core"
 
 describe FizzBuzz do
-  it "will return an array starting with 1 and ending with an entered number, replacing mult of 3 with Fizz" do
-    @fizzbuzz = FizzBuzz.new(10)
-    expect(@fizzbuzz.arrayify).to eq [1,2,"Fizz",4,5,"Fizz",7,8,"Fizz",10]
+  it "will return an array replacing mult of 3 with Fizz, 5 with Buzz" do
+    @fizzbuzz = FizzBuzz.new(14)
+    expect(@fizzbuzz.arrayify).to eq [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz", 11, "Fizz", 13, 14]
   end
 end
