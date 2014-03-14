e3eb4fcd7d2d393366317da83235994ec57837bc
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-14 15:47:30 -0600

    Refactor to move duplicate rspec items into a beautiful before do

--- spec/key_value_store_spec.rb
+++ spec/key_value_store_spec.rb
@@ -2,32 +2,24 @@ require "rspec/core"
 require "key_value_store"
 
 describe KeyValueStore do
+  before do
+    @its_a_hash = KeyValueStore.new
+    @its_a_hash.add_kv_pair("some key", "some pair")
+    @its_a_hash.add_kv_pair("another key", "another pair")
+  end
 
   it "Allows the user to add a value stored under a key" do
-    @its_a_hash = KeyValueStore.new
-    @its_a_hash.add_kv_pair("some key", "some pair")
-    @its_a_hash.add_kv_pair("another key", "another pair")
-
-
     expect(@its_a_hash.the_hash["some key"]).to eq "some pair"
   end
-  it "Allows the user to delete a single key/value pair" do
-    @its_a_hash = KeyValueStore.new
-    @its_a_hash.add_kv_pair("some key", "some pair")
-    @its_a_hash.add_kv_pair("another key", "another pair")
 
+  it "Allows the user to delete a single key/value pair" do
     @its_a_hash.delete_pair("some key")
     expect(@its_a_hash.the_hash["some key"]).to eq nil
-
-
   end
+
   it "Allows the user to get a list of all the keys" do
-    @its_a_hash = KeyValueStore.new
-    @its_a_hash.add_kv_pair("some key", "some pair")
-    @its_a_hash.add_kv_pair("another key", "another pair")
-
-
-    expect(@its_a_hash.list_keys).to eq ["some key", "another key"]
+   expect(@its_a_hash.list_keys).to eq ["some key", "another key"]
   end
+  
   it "Allows the user to clear the store of all keys"
 end
