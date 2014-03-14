9391be0d0e78109e8097a26f8374b071d00c78b1
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-14 16:21:24 -0600

    Sketched out the tests for odd_even

---
+++ spec/odd_even_spec.rb
@@ -0,0 +1,6 @@
+describe do
+  it "The class will return the numbers starting with 1 and ending with the number that it was initialized with when the run method is called."
+  it "For example, if I pass in 10, then the program will return an array containing 1 through 10."
+  it "If the number is even then the program will replace the number in the array with the text 'Even'."
+  it "If the number is odd, then the program will replace the number in the array with the text 'Odd'."
+end
