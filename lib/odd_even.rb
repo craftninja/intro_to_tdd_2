ec7ed9acf4354459ab70f553a349d2d2cec42e28
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-14 17:00:48 -0600

    Odd even propogates an array of 1 to the number initialized with

---
+++ lib/odd_even.rb
@@ -0,0 +1,11 @@
+class OddEven
+  def initialize (max_num)
+    @max_num = max_num
+  end
+
+  def run
+    @array = 1.upto(@max_num)
+    @array
+  end
+
+end
--- spec/odd_even_spec.rb
+++ spec/odd_even_spec.rb
@@ -1,6 +1,18 @@
-describe do
-  it "The class will return the numbers starting with 1 and ending with the number that it was initialized with when the run method is called."
-  it "For example, if I pass in 10, then the program will return an array containing 1 through 10."
+require 'odd_even'
+
+describe OddEven do
+  it "The class will return the numbers starting with 1 and ending with the number that it was initialized with when the run method is called." do
+    odd_even = OddEven.new(5)
+    expected = [1,2,3,4,5]
+    actual = odd_even.run.to_a
+    expect(actual).to eq expected
+  end
+  it "For example, if I pass in 10, then the program will return an array containing 1 through 10." do
+    odd_even = OddEven.new(10)
+    expected = [1,2,3,4,5,6,7,8,9,10]
+    actual = odd_even.run.to_a
+    expect(actual).to eq expected
+  end
   it "If the number is even then the program will replace the number in the array with the text 'Even'."
   it "If the number is odd, then the program will replace the number in the array with the text 'Odd'."
 end
