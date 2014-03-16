2def5ab7c36fc3c05d2c6ab8e8b2affbc0a82c84
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-16 12:53:03 -0600

    A normally nice dog will now bark if you poke her 3x

--- lib/angry_dog.rb
+++ lib/angry_dog.rb
@@ -1,6 +1,8 @@
 class AngryDog
   def poke (how_many_pokes = 1)
-    if how_many_pokes > 0
+    if how_many_pokes >=3
+      "woof!"
+    elsif how_many_pokes > 0
       "grrrrr"
     end
   end
--- spec/angry_dog_spec.rb
+++ spec/angry_dog_spec.rb
@@ -5,4 +5,8 @@ describe AngryDog do
     branwyn = AngryDog.new
     expect("grrrrr").to eq branwyn.poke(1)
   end
+  it "will bark if you poke her three times" do
+    branwyn = AngryDog.new
+    expect("woof!").to eq branwyn.poke(3)
+  end
 end
