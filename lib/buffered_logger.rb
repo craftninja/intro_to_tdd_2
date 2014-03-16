f603b2071acfeaa878fdb18ed1239ab8f34ca8be
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-16 17:01:04 -0600

    Buffered logger writes logs to a file after 5 logs have been collected

--- lib/buffered_logger.rb
+++ lib/buffered_logger.rb
@@ -11,6 +11,13 @@ class BufferedLogger
   def new_log(message, date)
     @current_backlog_text = "#{date.strftime(format = '%FT%R')} - #{message}"
     @current_backlog_array << @current_backlog_text
+    if @current_backlog_array.length == 5
+      File.open @filename, 'a' do |f|
+        @current_backlog_array.each do |current_backlog_text|
+          f.write "#{current_backlog_text}\n"
+        end
+      end
+    end
   end
 
   def current_backlog
--- spec/buffered_logger_spec.rb
+++ spec/buffered_logger_spec.rb
@@ -26,7 +26,18 @@ describe BufferedLogger do
     expect(actual).to eq expected
   end
 
-  it "writes stuff to a .txt file once there are 5 logs"
+  it "writes stuff to a .txt file once there are 5 logs" do
+    buff_log=BufferedLogger.new
+    buff_log.new_log("I coded in ruby all day... fun!", Date.new(2014, 3, 15))
+    buff_log.new_log("I hit a wall... time to make some tea.", Date.new(2014, 3, 16))
+    buff_log.new_log("I coded in ruby all day... fun!", Date.new(2014, 3, 17))
+    buff_log.new_log("I hit a wall... time to make some tea.", Date.new(2014, 3, 18))
+    buff_log.new_log("I coded in ruby all day... fun!", Date.new(2014, 3, 19))
+    expected = "2014-03-15T00:00 - I coded in ruby all day... fun!\n2014-03-16T00:00 - I hit a wall... time to make some tea.\n2014-03-17T00:00 - I coded in ruby all day... fun!\n2014-03-18T00:00 - I hit a wall... time to make some tea.\n2014-03-19T00:00 - I coded in ruby all day... fun!\n"
+    actual = File.read buff_log.filename
+    expect(actual).to eq expected
+  end
+
   it "writes more stuff to the text file once there are 5 more logs (10 total, 15 total...)"
 
 end
