a38c8e4cbf5467f070f9ae1d40205124c9dc8487
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-16 15:53:34 -0600

    Buffered Logger records multiple logs in an array
    Also updated the spec to be more accurate

--- spec/buffered_logger_spec.rb
+++ spec/buffered_logger_spec.rb
@@ -9,16 +9,23 @@ describe BufferedLogger do
     expect(actual).to eq expected
   end
 
-  it "records a log" do
+  it "records a log with time and date" do
     buff_log=BufferedLogger.new
-    buff_log.new_log("I coded in ruby all day... fun!", Date.new(2011, 4, 25))
-    expected = ["2011-04-25T00:00 - I coded in ruby all day... fun!"]
+    buff_log.new_log("I coded in ruby all day... fun!", Date.new(2014, 3, 15))
+    expected = ["2014-03-15T00:00 - I coded in ruby all day... fun!"]
     actual = buff_log.current_backlog
     expect(actual).to eq expected
   end
 
-  it "records a log with a time and date (one line for each log)"
-  it "records up to 4 logs with time and date"
+  it "records mult logs with a time and date" do
+    buff_log=BufferedLogger.new
+    buff_log.new_log("I coded in ruby all day... fun!", Date.new(2014, 3, 15))
+    buff_log.new_log("I hit a wall... time to make some tea.", Date.new(2014, 3, 16))
+    expected = ["2014-03-15T00:00 - I coded in ruby all day... fun!", "2014-03-16T00:00 - I hit a wall... time to make some tea."]
+    actual = buff_log.current_backlog
+    expect(actual).to eq expected
+  end
+
   it "writes stuff to a .txt file once there are 5 logs"
   it "writes more stuff to the text file once there are 5 more logs (10 total, 15 total...)"
 
