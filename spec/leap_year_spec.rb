a32e176c053d2bb47e13afd996184f16bcd0540d
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-16 22:05:11 -0600

    Sketched out rspec test for Leap Year

---
+++ spec/leap_year_spec.rb
@@ -0,0 +1,8 @@
+require 'leap_year'
+
+describe LeapYear do
+  it "is not initialized with anything special."
+  it "method called yes? will result leap year or not. 2001 is not a leap year but 1996 is a leap year"
+  it "is not leap year if it is divisible by 100 (1900)"
+  it "is leap year if divisible by 400 (2000 is a leap year)"
+end
