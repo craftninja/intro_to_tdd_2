1a805cf880847ced3f0f4a7a8c87141316b263d1
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-23 15:50:41 -0600

    Numeric Calc subtracts 3 from 9

--- lib/numeric_calc.rb
+++ lib/numeric_calc.rb
@@ -2,5 +2,8 @@ class NumericCalc
   def add (number_a,number_b)
     number_a+number_b
   end
+  def subtract(number_a,number_b)
+    number_a-number_b
+  end
 
 end
--- spec/numberic_calc_spec.rb
+++ spec/numberic_calc_spec.rb
@@ -7,5 +7,11 @@ describe NumericCalc do
     expected = 9
     expect(actual).to eq expected
   end
+  it "subtracts 3 from 9" do
+    calculator = NumericCalc.new
+    actual = calculator.subtract(9,3)
+    expected = 6
+    expect(actual).to eq expected
+  end
 
 end
